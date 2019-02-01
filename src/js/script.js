8ca12e6bf8d958cf0560ec471879ba28f9481ca1
alex <hamster2240@gmail.com>
2019-02-01 11:44:37 +0300

    Food car blocks isolation

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-var cards = document.querySelectorAll('.cards__item');
-var buyLink = document.querySelectorAll('.cards__text-link');
-var cardText = document.querySelectorAll('.cards__text');
-var stuff = document.querySelectorAll('.cards__text--stuffing');
-var buy = document.querySelectorAll('.cards__text-buy');
-var outOfStock = document.querySelectorAll('.cards__text--out-of-stock');
+var cards = document.querySelectorAll('.food-card__wrapper');
+var buyLink = document.querySelectorAll('.food-card__text-link');
+var cardText = document.querySelectorAll('.food-card__text');
+var stuff = document.querySelectorAll('.food-card__text--stuffing');
+var buy = document.querySelectorAll('.food-card__text-buy');
+var outOfStock = document.querySelectorAll('.food-card__text--out-of-stock');
 
 function clickActive() {
   cards.forEach(function(card, i) {
@@ -13,10 +13,10 @@ function clickActive() {
       return false;
     } else {
       card.addEventListener('click', function() {
-        this.classList.toggle('food-card--selected')
-        this.classList.toggle('food-card--default')
-        this.classList.toggle('food-card--selected-hover')
-        this.classList.remove('food-card--hover')
+        this.classList.toggle('food-card__wrapper--selected')
+        this.classList.toggle('food-card__wrapper--default')
+        this.classList.toggle('food-card__wrapper--selected-hover')
+        this.classList.remove('food-card__wrapper--hover')
         weight.classList.toggle('food-card__weight--selected')
         weight.classList.toggle('food-card__weight--selected-hover')
         stuff[i].classList.toggle('js-inactive')
@@ -32,35 +32,35 @@ clickCheckAndHover()
 function clickCheckAndHover() {
   cards.forEach(function(card) {
     var weight = card.querySelector('.food-card__weight')
-    if (card.classList.contains('food-card--selected')) {
+    if (card.classList.contains('food-card__wrapper--selected')) {
         card.addEventListener('click', function() {
           this.querySelector('.food-card__promo-food').style.display = 'block'
           this.querySelector('.food-card__promo-cat').style.display = 'none'
         })
         card.addEventListener('mouseenter', function() {
-          this.classList.remove('food-card--hover')
-          this.classList.add('food-card--selected-hover')
+          this.classList.remove('food-card__wrapper--hover')
+          this.classList.add('food-card__wrapper--selected-hover')
           weight.classList.add('food-card__weight--selected-hover')
           this.querySelector('.food-card__promo-food').style.display = 'none'
           this.querySelector('.food-card__promo-cat').style.display = 'block'
         })
       card.addEventListener('mouseleave', function() {
-        this.classList.remove('food-card--selected-hover')
+        this.classList.remove('food-card__wrapper--selected-hover')
         weight.classList.remove('food-card__weight--selected-hover')
         this.querySelector('.food-card__promo-food').style.display = 'block'
         this.querySelector('.food-card__promo-cat').style.display = 'none'
       })
-    } else if (card.classList.contains('food-card--default')) {
+    } else if (card.classList.contains('food-card__wrapper--default')) {
       card.addEventListener('mouseenter', function() {
-        this.classList.add('food-card--hover')
-        this.classList.remove('food-card--selected-hover')
+        this.classList.add('food-card__wrapper--hover')
+        this.classList.remove('food-card__wrapper--selected-hover')
         weight.classList.add('food-card__weight--default-hover')
         weight.classList.remove('food-card__weight--selected-hover')
         this.querySelector('.food-card__promo-food').style.display = 'block'
         this.querySelector('.food-card__promo-cat').style.display = 'none'
       })
       card.addEventListener('mouseleave', function() {
-        this.classList.remove('food-card--hover')
+        this.classList.remove('food-card__wrapper--hover')
         weight.classList.remove('food-card__weight--default-hover')
       })
     } else {
